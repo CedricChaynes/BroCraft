6f7faf1e0c5f8a3c3782b8454f6da3962e5e523a
CedricChaynes <cedric.chaynes@outlook.com>
2019-05-20 18:59:13 +0200

    add begining routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show] do
+    resources :tools, except: [:destroy]
+    resources :bookings, except: [:destroy]
+  end
+
+  resources :bookings, only: [:destroy]
+  resources :tools, only: [:destroy]
+
 end
