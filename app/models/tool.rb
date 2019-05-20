4bf68f99a467a3dcdad23f2081b7e090bac26669
CedricChaynes <cedric.chaynes@outlook.com>
2019-05-20 16:46:31 +0200

    fixe tool model

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,9 +1,10 @@
 class Tool < ApplicationRecord
-  belong_to :user
+  CATEGORY = ['outillage à main', 'outillage électroportatif', 'outillage spécialisé', 'protection', 'équipement de chantier', "machine d'atelier"]
+
+  belongs_to :user
   has_many :bookings
   validates :name, presence: true
   validates :description, presence: true
-  validates :price_per_day, presence: true
-  validates :category, presence: true
-  validates :price_per_day, numericality: { greater_than_or_equal_to: 0 }
+  validates :price_per_day, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :category, presence: true, inclusion: { in: CATEGORY }
 end
