d4d5f3e72e619c855120799591f82f61f6244dc1
Adele Soullier <adele.soullier@gmail.com>
2019-05-21 11:16:49 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show] do
     resources :tools, except: [:destroy]
-    resources :bookings, except: [:destroy]
+    resources :bookings, except: [:destroy, :new]
   end
-
-  resources :bookings, only: [:destroy]
-  resources :tools, only: [:destroy]
+  resources :tools, only: [:destroy, :index]
 
 end
