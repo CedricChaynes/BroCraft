c76f053af8ff23c35e613cc0cc93e26c36079f25
Adele Soullier <adele.soullier@gmail.com>
2019-05-21 11:24:35 +0200

    add tool show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show] do
-    resources :tools, except: [:destroy]
+    resources :tools, except: [:destroy, :show]
     resources :bookings, except: [:destroy, :new]
   end
-  resources :tools, only: [:destroy, :index]
+  resources :tools, only: [:destroy, :index, :show]
 
 end
