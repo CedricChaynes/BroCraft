5580c1d902a74ccc10b698950aa7f2cc0af5d8a1
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-05-21 18:24:22 +0200

    revisited seeder and price for views

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,19 @@ require 'json'
 require 'nokogiri'
 require 'faker'
 
+queries = %w[hand-tools automotive-tools home-and-garden air-tools power-tools metal-working-tools]
+baseurl = 'https://www.toolplanet.com/category/'
+img_url_list = []
+queries.each do |query|
+  url = "#{baseurl}#{query}"
+  response = open(baseurl).read
+  doc = Nokogiri::HTML(response)
+  doc.search('.image a img').each do |elem|
+    img_url_list << elem.attribute('src').value
+  end
+end
+100.times { img_url_list.shuffle! }
+
 def generate_French_mobile_number
   "#{%w[+33 (+33) 0].sample}#{rand(6..7)}#{rand.to_s[2..9]}"
 end
@@ -24,19 +37,22 @@ CATEGORIES = ['outillage à main', 'outillage électroportatif',
               'outillage spécialisé', 'protection', 'équipement de chantier',
               "machine d'atelier"]
 
-10.times do
-  user = User.create!(username: Faker::Internet.username, first_name: Faker::Name.first_name,
-         last_name: Faker::Name.last_name, email: Faker::Internet.email,
-         password: Faker::Internet.password, mobile: generate_French_mobile_number,
+count = 1
+while count < 10
+  user = User.create!(username: "user#{count}", first_name: Faker::Name.first_name,
+         last_name: Faker::Name.last_name, email: "user#{count}@gmail.com",
+         password: '123456', mobile: generate_French_mobile_number,
          address: Faker::Address.full_address, avatar_url: generate_image_url)
   rand(1..5).times do
     tool = Tool.new(name: Faker::ElectricalComponents.electromechanical,
               description: Faker::Lorem.paragraph_by_chars(256, false),
               category: CATEGORIES.sample, price_per_day: generate_price,
-              photo_url: generate_image_url)
+              photo_url: img_url_list.last)
     tool.owner = user
     tool.save!
+    img_url_list.pop
   end
+  count += 1
 end
 
 100.times do |variable|
