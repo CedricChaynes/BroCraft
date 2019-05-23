f1ab583a2a5d4e0af37c6d070c4db23ff5149bd1
CedricChaynes <cedric.chaynes@outlook.com>
2019-05-23 20:40:43 +0200

    frooooooont

--- app/javascript/packs/animateClaim.js
+++ app/javascript/packs/animateClaim.js
@@ -4,8 +4,8 @@ const animateClaim = () => {
   const element = document.querySelector(".changing");
   if (element) {
     var options = {
-    strings: ["une perceuse à bois", "un décapeur thermique", "un pistolet à colle", "une meuleuse à poutre", "une scie sauteuse", "une élagueuse de jardin", "une tondeuse à gazon", "une grande échelle", "une carotteuse", "un marteau arrache clou", "une défonceuse"],
-    typeSpeed: 20,
+    strings: ["une perceuse à bois", "un décapeur thermique", "un pistolet à colle", "une meuleuse à poutre", "une scie sauteuse", "un Pokémon", "une élagueuse de jardin", "une meilleure tireuse à bière que chez AirBnMousse", "une tondeuse à gazon", "une grande échelle", "une carotteuse", "une meilleure tireuse à bière que chez AirBnMousse", "un marteau arrache clou", "une défonceuse"],
+    typeSpeed: 30,
     loop: true,
     shuffle: true,
     startDelay: 5,
